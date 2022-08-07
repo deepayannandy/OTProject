0f763d35b01ffc696c74eb9e36452afc131c7733
Deepayan Nandy <deepayan.622@gmail.com>
2022-08-08 04:12:12 +0530

    v1.7

--- apps/projectviewer.py
+++ apps/projectviewer.py
@@ -69,12 +69,13 @@ def showEMPDetails(otdb,emp):
     col3.metric("Contact No.", em["mobileNo"])
 
 def showWODetails(otdb,wo):
-    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+
     res = otdb.db.collection("Workorders").where("wo", "==", wo).get()
     wo=res[0].to_dict()
     col0, col00,col000= st.columns((3, 1,1))
     col0.title("Work Details of : " + wo["wo"])
     with col00:
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
         qr.add_data("http://3.95.56.247:8080/wo/"+wo["wo"])
         qr.make(fit=True)
         img = qr.make_image()
@@ -83,6 +84,7 @@ def showWODetails(otdb,wo):
         st.image(openImage(imgpath))
         st.write("http://3.95.56.247:8080/wo/"+wo["wo"])
     with col000:
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
         qr.add_data("http://3.95.56.247:8080/invoice/"+wo["wo"])
         qr.make(fit=True)
         img = qr.make_image()
